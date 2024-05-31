d931a70214451fbc68fccd0bfafc801340774548
Marcelle Vargas <marcellecode@gmail.com>
2024-05-31 18:22:28 -0300

    [feat]- create a input component

---
+++ weather-app/src/components/InputLocation/InputLocation.js
@@ -0,0 +1,9 @@
+import "./InputLocation.css";
+
+export default function InputLocation({ location }) {
+    return (
+        <div className="location-container">
+            <h1>{location}</h1>
+        </div>
+    );
+}
