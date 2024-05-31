3d0f4975a5240a31f8965c222c80ced72dc4daa2
Marcelle Vargas <marcellecode@gmail.com>
2024-05-31 19:53:03 -0300

    [style]- update style of InputLocation component

--- weather-app/src/components/InputLocation/InputLocation.js
+++ weather-app/src/components/InputLocation/InputLocation.js
@@ -3,6 +3,7 @@ import "./InputLocation.css";
 export default function InputLocation({ state, stateCode }) {
     return (
         <div className="location-container">
+            <a href="" className="icon" data-icon="("></a>
             <span>{stateCode}</span><input defaultValue={state}/>
         </div>
     );
